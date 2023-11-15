44ec23d68d2b3c0a3727101bc9267e231425e376
gajorKhabo <118203937+arnabishakh@users.noreply.github.com>
2023-11-15 12:39:56 +0600

    Add files via upload

---
+++ Green-Dhaka-Web-Tech-Project/Green Dhaka/View/AdminHomeVisit.php
@@ -0,0 +1,102 @@
+<?php
+
+
+?>
+    <style>
+  
+         body {
+              background-image: url('../Images/white.jpg');
+              background-position: center;
+              background-size: cover;
+            }
+         a:link {
+                background-color: SeaGreen;
+                color: white;
+                padding: 5px 5px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+         }
+         a:visited {
+                background-color: SeaGreen;
+                color: white;
+                padding: 5px 5px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+         }
+         a:hover {
+                background-color: transparent;
+                color: Black;
+                padding: 5px 5px;
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+
+         }
+    </style>
+    <form method="post" action="../Controller/solarinsert.php">
+    <table border="0" width = "100%" height = "100%">
+            <tr >
+                <td height="5%" width="15%" align="center">
+                    <img src="../Images/logo.png" height="100" width="100" alt="Green Dhaka Logo">
+                </td>
+                <td align="center" height="5%"width="70%">
+                    <h1 style="color:SeaGreen;">  </h1> <br>
+                    <h1 style="color:SeaGreen;"> Green Dhaka </h1>
+                    <h4 style="color:SeaGreen;"> An initiative of Dhaka City Corporation </h4>
+                    <p style="color:SeaGreen;"> <b>  </b> </p>
+                    
+                </td>
+                <td >
+                <table border="0" align="center">
+                    <tr>
+                        <td> <img src="../Images/user.png" height="50" width="50" alt="User Logo">
+                        </td>
+                        <td align="right"> <br>
+                             <a href="adminDashboard.php">Dashboard</a>
+                             <a href="login.php"> Logout <a>
+                        </td>
+                    </tr>
+                
+                </table>
+                   
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3" align="center" height ="8%">
+                  
+
+                </td>
+                
+            </tr>
+            <tr>
+            <td>
+        
+            </td>
+                <td align ="center">
+                <h3 style="color:SeaGreen;"> <b> Home Visit Approval </b> </h3>
+                <br>
+                <h3 style="color:SeaGreen;"> Under Maintanance  </h3>
+                
+
+                </td>
+
+                <td>
+            </td>
+            </tr>
+           
+            <tr height ="5%" align="center">
+                <td>
+                </td>
+                <td>
+                <p style="color:white;background-color:SeaGreen;"> Call now : +88017xxxxxxx Email:greendhaka@gmail.com Address: Kuril,KA,187/0A </p>
+                
+                </td>
+                <td>
+                </td>
+            </tr>
+            
+
+    </table>
+    </form>
